346979c9fb71067269dbe024257954ce41c2b877
Fisheep1207 <mike881207@gmail.com>
2021-12-27 12:11:38 +0000

    pnls endpoint

--- index.js
+++ index.js
@@ -6,13 +6,10 @@ app.get('/', function (req, res) {
 	res.send("hello");
 })
 
-app.get('/sand_pnl', function (req, res) {
-// console.log("/reef_pnl")
-  res.sendFile("/home/qiuyuyang/python_monitor/sand_config.html");
-});
-
 app.use('/graphs', express.static('/home/qiuyuyang/python_monitor/graphs/html'))
 app.use('/graphs', serveIndex('/home/qiuyuyang/python_monitor/graphs/html'))
+app.use('/pnl_graphs', express.static('/home/qiuyuyang/python_monitor/pnls/html'))
+app.use('/pnl_graphs', serveIndex('/home/qiuyuyang/python_monitor/pnls/html'))
 
 app.listen(8888, "0.0.0.0", function () { 
   console.log('Example app listening on port 8888!');
