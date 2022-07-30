9ad83c81094336c15b37863fe5db95199b11dda5
RouChiou <moneyMonkeyYang@gmail.com>
2022-07-30 05:17:36 +0000

    new

--- index.js
+++ index.js
@@ -8,8 +8,10 @@ app.get('/', function (req, res) {
 
 app.use('/graphs', express.static('/home/qiuyuyang/python_monitor/graphs/html'))
 app.use('/graphs', serveIndex('/home/qiuyuyang/python_monitor/graphs/html'))
-app.use('/pnl_graphs', express.static('/home/qiuyuyang/python_monitor/pnls/html'))
-app.use('/pnl_graphs', serveIndex('/home/qiuyuyang/python_monitor/pnls/html'))
+app.use('/tri_graphs', express.static('/home/qiuyuyang/python_monitor/tri_graphs/html'))
+app.use('/tri_graphs', serveIndex('/home/qiuyuyang/python_monitor/tri_graphs/html'))
+// app.use('/pnl_graphs', express.static('/home/qiuyuyang/python_monitor/pnls/html'))
+// app.use('/pnl_graphs', serveIndex('/home/qiuyuyang/python_monitor/pnls/html'))
 
 app.listen(8888, "0.0.0.0", function () { 
   console.log('Example app listening on port 8888!');
